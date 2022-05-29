3df36c50543ed0277f894f7e26f5c7855ac88798
juansebas <juansebastiancastilloojeda@gmail.com>
2022-05-29 19:43:14 +0800

    1er commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+console.log('Happy hacking :)')
+
+async function fecthProve(){
+    
+}
