bff7146e276462c724aaaaf8ac1d21821ef45743
juansebas <juansebastiancastilloojeda@gmail.com>
2022-05-29 22:15:05 +0800

    primera respuesta del fetch correcta

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@
 
 console.log('Happy hacking :)')
 
+
+
+const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=Caracas,ve&appid=da7fbe90814d1c0df0952d23c1bd66d7`
+
 async function fecthProve(){
-    
+    const response = await window.fetch(baseUrl)
+    const resJson = response.json()
+    return resJson
 }
+
+fecthProve().then(
+    data => console.log(data)
+)
